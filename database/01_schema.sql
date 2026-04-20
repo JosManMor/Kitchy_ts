079739edd129ea054b966c557f1baf94cb2905f9
Novaxux <manuelova@outlook.es>
2026-04-20 15:05:29 -0600

    feat: mail in db schema

--- database/01_schema.sql
+++ database/01_schema.sql
@@ -8,6 +8,7 @@ CREATE TABLE roles (
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     username VARCHAR(100) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
     password TEXT NOT NULL,
     role_id INT NOT NULL DEFAULT 2,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
