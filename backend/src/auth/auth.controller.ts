39fd4d45c582c3bac122a3f04afd8002d032f96b
Novaxux <manuelova@outlook.es>
2026-04-26 00:04:20 -0600

    feat(auth): register, login, prisma

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,11 +1,25 @@
-import { Controller, Post } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  login() {
-    
+  @UsePipes(new ValidationPipe())
+  login(@Body() loginData: LoginDto) {
+    return this.authService.login(loginData);
   }
-
+  @Post('register')
+  register(@Body() registerData: RegisterDto) {
+    return this.authService.register(registerData);
+  }
 }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,7 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  UnauthorizedException,
+  ConflictException,
+} from '@nestjs/common';
+import { PrismaService } from 'prisma.service';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-    
-    
+  constructor(private prisma: PrismaService) {}
+
+  async register(data: RegisterDto) {
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    try {
+      const user = await this.prisma.users.create({
+        data: {
+          ...data,
+          password: hashedPassword,
+        },
+      });
+
+      const { password: _password, ...result } = user;
+      return result;
+    } catch (error: unknown) {
+      if (error instanceof Error && 'code' in error) {
+        if (error.code === 'P2002') {
+          throw new ConflictException('Email or username already exists');
+        }
+        throw error;
+      }
+    }
+  }
+
+  async login(data: LoginDto) {
+    const user = await this.prisma.users.findUnique({
+      where: { email: data.email },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const isPasswordValid = await bcrypt.compare(data.password, user.password);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const { password: _password, ...result } = user;
+    return result;
+  }
 }
