b19e864dab6a1edb18408e37f2dcca727fab869b
Novaxux <manuelova@outlook.es>
2026-04-26 16:36:09 -0600

    fix: prisma connection

--- backend/prisma.service.ts
+++ backend/prisma.service.ts
@@ -1,11 +1,24 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from './generated/prisma/client';
+import { PrismaClient } from 'generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    const connectionString = process.env.DATABASE_URL;
+
+    if (!connectionString) {
+      throw new Error('DATABASE_URL is not defined');
+    }
+
+    super({
+      adapter: new PrismaPg({ connectionString }),
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
